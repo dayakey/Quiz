6bdaa82b04d5fc11cee618d21769538936b2590c
dayakey <daya10key@gmail.com>
2022-11-18 02:39:36 +0600

    third commit

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def new_game():
             print(el)
         print("_________________")
         while True:
-            guess = input("Please enter answer (A, B, C, D)").upper()
+            guess = input("Please enter answer (A, B, C, D):").upper()
             if guess != "A" and guess != "B" and guess != "C" and guess != "D":
                 print("Enter only A, B, C, D")
                 print("_______________")
@@ -34,6 +34,7 @@ def new_game():
         questions_num += 1
 
     display_score(correct_answers, len(guesses))
+    new_questions()
 
 
 def check_answer(answer, guess):
@@ -52,7 +53,7 @@ def display_score(correct_ans, guesses):
 
 def play_again():
     while True:
-        response = input("Do you want to play again? (YES OR NO)").upper()
+        response = input("Do you want to play again? (YES OR NO):").upper()
         if response != "YES" and response != "NO":
             print("Enter only YES or NO")
         else:
@@ -66,8 +67,50 @@ def play_again():
 def main():
     while play_again():
         new_game()
-        print("Bye")
+
+
+def admin():
+    while True:
+        response = input("Are you admin? And do you want to change quiz? (YES or NO):").upper()
+        if response != "YES" and response != "NO":
+            print("Enter only YES or NO")
+        else:
+            break
+    correct_password = "12345678"
+    if response == "YES":
+        password = input("Enter the admins' password (you have only 1 chance):")
+        if password == correct_password:
+            return True
+        else:
+            print("Your passwort is not correct")
+    elif response == "NO":
+        main()
+
+
+def new_questions():
+    while admin():
+        response_2 = input("Do you want to add new question? (YES or NO):").upper()
+        if response_2 != "YES" and response_2 != "NO":
+            print("Enter only YES or NO")
+        elif response_2 == "YES":
+            new_question = input("Enter the new question:").upper()
+            new_options = []
+            options_abcd = "ABCD"
+            for i in range(4):
+                new_option = input("Enter the new option:")
+                new_options.append(options_abcd[i] + ". " + new_option)
+            options.append(new_options)
+            while True:
+                option_for_new_question = input("Enter the correct option for new question (A, B, C, D):").upper()
+                if option_for_new_question != "A" and option_for_new_question != "B" and option_for_new_question != "C" and option_for_new_question != "D":
+                    print("Enter only A, B, C, D")
+                else:
+                    break
+            questions.setdefault(new_question, option_for_new_question.upper())
+        else:
+            print("Bye")
+            break
 
 
 new_game()
-main()
+print("Bye!")
